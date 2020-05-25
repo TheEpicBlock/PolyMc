29bc9f1232b597791ccfc5a5ddee84674ca97287
TheEpicBlock <61842090+TheEpicBlock@users.noreply.github.com>
2020-05-25 09:47:18 +0200

    Set up the classes and stuff

---
+++ src/main/java/io/github/theepicblock/polymc/PolyMc.java
@@ -0,0 +1,11 @@
+package io.github.theepicblock.polymc;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PolyMc implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
