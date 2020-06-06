09f7398919a0c590d41b9b17aad4a4995ee2f2e5
TheEpicBlock <61842090+TheEpicBlock@users.noreply.github.com>
2020-06-06 22:24:35 +0200

    Make item replacement functional

--- src/main/java/io/github/theepicblock/polymc/api/PolyManager.java
+++ src/main/java/io/github/theepicblock/polymc/api/PolyManager.java
@@ -1,6 +1,8 @@
 package io.github.theepicblock.polymc.api;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,5 +17,12 @@ public class PolyManager {
      */
     public static void registerItem(Item item, ItemPoly poly) {
         itemPolys.put(item, poly);
+    }
+
+    public static ItemStack getClientItem(ItemStack serverItem) {
+        if (serverItem.isEmpty()) {
+            return serverItem;
+        }
+        return new ItemStack(Items.STICK, serverItem.getCount());
     }
 }
---
+++ src/main/java/io/github/theepicblock/polymc/mixins/item/PacketByteBufMixin.java
@@ -0,0 +1,19 @@
+package io.github.theepicblock.polymc.mixins.item;
+
+import io.github.theepicblock.polymc.api.PolyManager;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.PacketByteBuf;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PacketByteBuf.class)
+public class PacketByteBufMixin {
+    @ModifyVariable(method = "writeItemStack(Lnet/minecraft/item/ItemStack;)Lnet/minecraft/util/PacketByteBuf;", at = @At("HEAD"))
+    public ItemStack writeItemStackValueOverwrite(ItemStack itemStack) {
+        return PolyManager.getClientItem(itemStack);
+    }
+}
