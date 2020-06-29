47bc749d77e6687a175e8d44394c13204ca3fa6c
TheEpicBlock <61842090+TheEpicBlock@users.noreply.github.com>
2020-06-29 19:30:23 +0200

    Port to 1.16.1

--- src/main/java/io/github/theepicblock/polymc/mixins/ServerStartMixin.java
+++ src/main/java/io/github/theepicblock/polymc/mixins/ServerStartMixin.java
@@ -13,8 +13,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
 public class ServerStartMixin {
-    @Inject(method = "createWorlds(Lnet/minecraft/world/WorldSaveHandler;Lnet/minecraft/world/level/LevelProperties;Lnet/minecraft/world/level/LevelInfo;Lnet/minecraft/server/WorldGenerationProgressListener;)V", at = @At("HEAD"))
-    public void createWorlds(WorldSaveHandler worldSaveHandler, LevelProperties properties, LevelInfo levelInfo, WorldGenerationProgressListener worldGenerationProgressListener, CallbackInfo ci) {
+    @Inject(method = "createWorlds(Lnet/minecraft/server/WorldGenerationProgressListener;)V", at = @At("HEAD"))
+    public void createWorlds(WorldGenerationProgressListener worldGenerationProgressListener, CallbackInfo ci) {
         PolyMc.generatePolyMap();
     }
 }
--- src/main/java/io/github/theepicblock/polymc/mixins/item/PacketByteBufMixin.java
+++ src/main/java/io/github/theepicblock/polymc/mixins/item/PacketByteBufMixin.java
@@ -2,7 +2,7 @@ package io.github.theepicblock.polymc.mixins.item;
 
 import io.github.theepicblock.polymc.PolyMc;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.PacketByteBuf;
+import net.minecraft.network.PacketByteBuf;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PacketByteBuf.class)
 public class PacketByteBufMixin {
-    @ModifyVariable(method = "writeItemStack(Lnet/minecraft/item/ItemStack;)Lnet/minecraft/util/PacketByteBuf;", at = @At("HEAD"))
+    @ModifyVariable(method = "writeItemStack(Lnet/minecraft/item/ItemStack;)Lnet/minecraft/network/PacketByteBuf;", at = @At("HEAD"))
     public ItemStack writeItemStackValueOverwrite(ItemStack itemStack) {
         return PolyMc.getMap().getClientItem(itemStack);
     }
