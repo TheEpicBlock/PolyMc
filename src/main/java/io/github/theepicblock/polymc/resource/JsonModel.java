ac5ba6007668bffc5577ad6b0cba7d3e2a8c82e8
TheEpicBlock <61842090+TheEpicBlock@users.noreply.github.com>
2020-07-05 22:22:20 +0200

    Sort CMD values so they display properly

--- src/main/java/io/github/theepicblock/polymc/resource/JsonModel.java
+++ src/main/java/io/github/theepicblock/polymc/resource/JsonModel.java
@@ -1,6 +1,7 @@
 package io.github.theepicblock.polymc.resource;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -14,6 +15,14 @@ public class JsonModel {
             overrides = new ArrayList<>();
         }
         overrides.add(e);
+        Collections.sort(overrides,(o1,o2) -> {
+            if (o1.predicate.size() > 0 && o2.predicate.size() > 0) {
+                int i1 = o1.predicate.values().iterator().next();
+                int i2 = o2.predicate.values().iterator().next();
+                return i1 - i2;
+            }
+            return 0;
+        });
     }
 
     public static class Override {
