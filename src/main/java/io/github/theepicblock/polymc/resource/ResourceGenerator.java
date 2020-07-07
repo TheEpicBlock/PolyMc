ab255575306e029e3df63b8dcf6601c602ff9d31
TheEpicBlock <61842090+TheEpicBlock@users.noreply.github.com>
2020-07-07 22:34:16 +0200

    Add text files to resource packs

--- src/main/java/io/github/theepicblock/polymc/resource/ResourceGenerator.java
+++ src/main/java/io/github/theepicblock/polymc/resource/ResourceGenerator.java
@@ -2,9 +2,13 @@ package io.github.theepicblock.polymc.resource;
 
 import io.github.theepicblock.polymc.PolyMc;
 import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Stream;
 
 public class ResourceGenerator {
     @SuppressWarnings("ResultOfMethodCallIgnored")
@@ -14,6 +18,8 @@ public class ResourceGenerator {
         resourceDir.mkdirs();
         Path path = resourceDir.toPath().toAbsolutePath();
         ResourcePackMaker pack = new ResourcePackMaker(path);
+
+        //Hooks for all itempolys
         PolyMc.getMap().getItemPolys().forEach((item, itemPoly) -> {
             try {
                 itemPoly.AddToResourcePack(item, pack);
@@ -22,6 +28,22 @@ public class ResourceGenerator {
                 e.printStackTrace();
             }
         });
+
+        //Get all lang files from all mods
+        for (ModContainer mod : FabricLoader.getInstance().getAllMods()) {
+            String modId = mod.getMetadata().getId();
+            Path langPath = mod.getPath(String.format("assets/%s/lang", modId));
+            if (!Files.exists(langPath)) continue;
+            try {
+                Stream<Path> pathStream = Files.list(langPath);
+                pathStream.forEach((langFile) -> {
+                    pack.copyAssetFromMod(modId, "lang/"+langPath.relativize(langFile));
+                });
+            } catch (IOException e) {
+                PolyMc.LOGGER.warning("Exception whilst copying lang files from " + modId);
+                e.printStackTrace();
+            }
+        }
 
         pack.saveAll();
     }
