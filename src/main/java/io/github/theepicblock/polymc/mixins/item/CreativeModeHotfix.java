6b9b9a10b7639c08364084f06da8b7619a84436a
TheEpicBlock <61842090+TheEpicBlock@users.noreply.github.com>
2020-07-09 22:04:52 +0200

    split CreativeModeHotfix and DesyncPatch

--- src/main/java/io/github/theepicblock/polymc/mixins/item/CreativeModeHotfix.java
+++ src/main/java/io/github/theepicblock/polymc/mixins/item/CreativeModeHotfix.java
@@ -1,42 +1,28 @@
 package io.github.theepicblock.polymc.mixins.item;
 
 import io.github.theepicblock.polymc.PolyMc;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.packet.c2s.play.CreativeInventoryActionC2SPacket;
 import net.minecraft.screen.PlayerScreenHandler;
-import net.minecraft.screen.ScreenHandler;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+/**
+ * When items are moved around by a creative mode player, the client just tells the server to set a stack to a specific item.
+ * This means that if the client thinks it's holding a stick, it will instruct the server to set the slot to a stick.
+ * Even if the stick is supposed to represent another item.
+ *
+ * My hacky solution:
+ * When a packet is sent to void a slot. The item previously in there gets set in "polyMCrecentlyVoided".
+ * Then when it tries to set a slot to an item. It first get's checked to see if the item it tries to set could be the poly of
+ * We also check if the client tries to set a slot to it's polyd version.
+ */
 @Mixin(ServerPlayNetworkHandler.class)
-public class ServerPlayNetworkHandlerMixin {
+public class CreativeModeHotfix {
     private ItemStack polyMCrecentlyVoided;
-    /**
-     * @reason Minecraft checks to see if the inventories are out of sync here. But when using PolyMC a desync is intentional. So we check here if the desync is actually a good desync or a bad desync.
-     */
-    @Redirect(method = "onClickWindow(Lnet/minecraft/network/packet/c2s/play/ClickWindowC2SPacket;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;areEqual(Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ItemStack;)Z"))
-    public boolean areEqualRedirect(ItemStack left, ItemStack right) {
-        return ItemStack.areEqual(left, PolyMc.getMap().getClientItem(right));
-    }
 
-    /*
-        When items are moved around by a creative mode player, the client just tells the server to set a stack to a specific item.
-        This means that if the client thinks it's holding a stick, it will instruct the server to set the slot to a stick.
-        Even if the stick is supposed to represent another item.
-
-        My hacky solution:
-        when a packet is sent to void a slot. The item previously in there gets set in "polyMCrecentlyVoided".
-        Then when it tries to set a slot to an item. It first get's checked to see if the item it tries to set could be the poly of
-        We also check if the client tries to set a slot to it's polyd version.
-        TODO add a config to disable this mess
-     */
-
-    /**
-     * @reason see comment block above
-     */
     @Redirect(method = "onCreativeInventoryAction(Lnet/minecraft/network/packet/c2s/play/CreativeInventoryActionC2SPacket;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/screen/PlayerScreenHandler;setStackInSlot(ILnet/minecraft/item/ItemStack;)V"))
     public void creativemodeSetSlotRedirect(PlayerScreenHandler screenHandler, int slot, ItemStack setStack) {
         ItemStack slotStack = screenHandler.getSlot(slot).getStack();
@@ -44,7 +30,7 @@ public class ServerPlayNetworkHandlerMixin {
             if (setStack.isEmpty()) {
                 polyMCrecentlyVoided = slotStack;
             } else {
-                if (ItemStack.areEqual(setStack,PolyMc.getMap().getClientItem(slotStack))) {
+                if (ItemStack.areEqual(setStack, PolyMc.getMap().getClientItem(slotStack))) {
                     //the item the client is trying to set is actually a the polyd version of the item in the same slot.
                     return;
                 }
---
+++ src/main/java/io/github/theepicblock/polymc/mixins/item/DesyncPatch.java
@@ -0,0 +1,26 @@
+package io.github.theepicblock.polymc.mixins.item;
+
+import io.github.theepicblock.polymc.PolyMc;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.packet.c2s.play.CreativeInventoryActionC2SPacket;
+import net.minecraft.screen.PlayerScreenHandler;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+/**
+ * Minecraft checks to see if the inventories are out of sync.
+ * But when using PolyMC a desync is intentional.
+ * So we check here if the desync is actually a good desync or a bad desync.
+ */
+@Mixin(ServerPlayNetworkHandler.class)
+public class DesyncPatch {
+
+    @Redirect(method = "onClickWindow(Lnet/minecraft/network/packet/c2s/play/ClickWindowC2SPacket;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;areEqual(Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ItemStack;)Z"))
+    public boolean areEqualRedirect(ItemStack left, ItemStack right) {
+        return ItemStack.areEqual(left, PolyMc.getMap().getClientItem(right));
+    }
+}
