32df9316ff008460529feeaa83652360d30a477b
TheEpicBlock <61842090+TheEpicBlock@users.noreply.github.com>
2020-07-24 19:34:01 +0200

    Allow fabric clients to join

---
+++ src/main/java/io/github/theepicblock/polymc/mixins/FabricRegistrySyncDisabler.java
@@ -0,0 +1,38 @@
+/*
+ * PolyMc
+ * Copyright (C) 2020-2020 TheEpicBlock_TEB
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; If not, see <https://www.gnu.org/licenses>.
+ */
+package io.github.theepicblock.polymc.mixins;
+
+import net.fabricmc.fabric.impl.registry.sync.RegistrySyncManager;
+import net.minecraft.network.Packet;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * Fabric has a system in place to prevent Fabric clients from joining if the registries don't match.
+ * This is normally useful, as you wouldn't want to join a server if you don't have the needed blocks.
+ * But with PolyMc it shouldn't matter what blocks you have.
+ */
+@Mixin(RegistrySyncManager.class)
+public class FabricRegistrySyncDisabler {
+    @Inject(method = "createPacket", at = @At("HEAD"), cancellable = true, remap = false)
+    private static void createPacketInject(CallbackInfoReturnable<Packet<?>> cir) {
+        cir.setReturnValue(null);
+    }
+}
