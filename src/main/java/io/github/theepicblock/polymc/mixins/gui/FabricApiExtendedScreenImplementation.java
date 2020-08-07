0d5f4c32fa455af90448c50927b11cfe3dc6c007
TheEpicBlock <61842090+TheEpicBlock@users.noreply.github.com>
2020-08-07 22:30:11 +0200

    Make guis work better

---
+++ src/main/java/io/github/theepicblock/polymc/mixins/gui/FabricApiExtendedScreenImplementation.java
@@ -0,0 +1,38 @@
+/*
+ * PolyMc
+ * Copyright (C) 2020-2020 TheEpicBlock_TEB
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; If not, see <https://www.gnu.org/licenses>.
+ */
+package io.github.theepicblock.polymc.mixins.gui;
+
+import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerFactory;
+import net.fabricmc.fabric.impl.screenhandler.Networking;
+import net.minecraft.network.packet.s2c.play.OpenScreenS2CPacket;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(Networking.class)
+public class FabricApiExtendedScreenImplementation {
+    /**
+     * @author TheEpicBlock
+     * @reason The vanilla client can't understand the custom packet
+     */
+    @Overwrite
+    public static void sendOpenPacket(ServerPlayerEntity player, ExtendedScreenHandlerFactory factory, ScreenHandler handler, int syncId) {
+        player.networkHandler.sendPacket(new OpenScreenS2CPacket(syncId,handler.getType(),factory.getDisplayName()));
+    }
+}
