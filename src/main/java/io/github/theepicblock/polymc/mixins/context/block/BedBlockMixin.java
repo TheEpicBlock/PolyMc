4813c671d969207b650e4ba6bbb623a74dc3f017
TheEpicBlock <git.teb@theepicblock.nl>
2021-01-09 22:50:51 +0100

    Add docs for BedBlockMixin

--- src/main/java/io/github/theepicblock/polymc/mixins/context/block/BedBlockMixin.java
+++ src/main/java/io/github/theepicblock/polymc/mixins/context/block/BedBlockMixin.java
@@ -31,13 +31,25 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+/**
+ * In the {@link BedBlock#onBreak(World, BlockPos, BlockState, PlayerEntity)} method, there is a call to create a WorldEvent for the breakage.
+ * Normally the remapping of that would be caught by {@link io.github.theepicblock.polymc.mixins.block.BlockPolyImplementation} but that method doesn't respect individuals their PolyMaps.
+ */
 @Mixin(BedBlock.class)
 public class BedBlockMixin {
+	/**
+	 * Removes the call to {@link World#syncWorldEvent(PlayerEntity, int, BlockPos, int)} so it can be replaced
+	 * @see #worldEventReplacement(World, BlockPos, BlockState, PlayerEntity, CallbackInfo, BedPart, BlockPos, BlockState)
+	 */
 	@Redirect(method = "onBreak", at = @At(value = "INVOKE",target = "Lnet/minecraft/world/World;syncWorldEvent(Lnet/minecraft/entity/player/PlayerEntity;ILnet/minecraft/util/math/BlockPos;I)V"))
 	public void worldEventDisabler(World world, PlayerEntity player, int eventId, BlockPos pos, int data) {
 		//Disabled
 	}
 
+	/**
+	 * Replaces the call to {@link World#syncWorldEvent(PlayerEntity, int, BlockPos, int)} with a call to {@link PacketReplacementUtil#syncWorldEvent(World, PlayerEntity, int, BlockPos, BlockState)}
+	 * to respect different PolyMaps
+	 */
 	@Inject(method = "onBreak", at = @At(value = "INVOKE",target = "Lnet/minecraft/world/World;syncWorldEvent(Lnet/minecraft/entity/player/PlayerEntity;ILnet/minecraft/util/math/BlockPos;I)V"),
 		locals = LocalCapture.CAPTURE_FAILHARD)
 	public void worldEventReplacement(World world, BlockPos pos, BlockState state, PlayerEntity player, CallbackInfo ci, BedPart var5, BlockPos var6, BlockState var7) {
