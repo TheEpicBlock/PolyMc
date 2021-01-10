f415ba2bf466f06b5df3160111d78c9a8f972cc9
TheEpicBlock <git.teb@theepicblock.nl>
2021-01-10 20:24:47 +0100

    Remove locals from BlockMixin as it wasn't needed

--- src/main/java/io/github/theepicblock/polymc/mixins/context/block/BlockMixin.java
+++ src/main/java/io/github/theepicblock/polymc/mixins/context/block/BlockMixin.java
@@ -28,7 +28,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 /**
  * In the {@link Block#onBreak(World, BlockPos, BlockState, PlayerEntity)} method, there is a call to create a WorldEvent for the breakage.
@@ -49,8 +48,7 @@ public class BlockMixin {
 	 * Replaces the call to {@link World#syncWorldEvent(PlayerEntity, int, BlockPos, int)} with a call to {@link PacketReplacementUtil#syncWorldEvent(World, PlayerEntity, int, BlockPos, BlockState)}
 	 * to respect different PolyMaps
 	 */
-	@Inject(method = "onBreak", at = @At(value = "INVOKE",target = "Lnet/minecraft/world/World;syncWorldEvent(Lnet/minecraft/entity/player/PlayerEntity;ILnet/minecraft/util/math/BlockPos;I)V"),
-			locals = LocalCapture.CAPTURE_FAILHARD)
+	@Inject(method = "onBreak", at = @At(value = "INVOKE",target = "Lnet/minecraft/world/World;syncWorldEvent(Lnet/minecraft/entity/player/PlayerEntity;ILnet/minecraft/util/math/BlockPos;I)V"))
 	public void worldEventReplacement(World world, BlockPos pos, BlockState state, PlayerEntity player, CallbackInfo ci) {
 		PacketReplacementUtil.syncWorldEvent(world, player, 2001, pos, state);
 	}
