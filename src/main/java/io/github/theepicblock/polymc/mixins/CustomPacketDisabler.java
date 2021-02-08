160dc4f9db83cf22495566765377605a9854a954
TheEpicBlock <git.teb@theepicblock.nl>
2021-02-08 20:04:45 +0100

    Make CustomPacketDisabler dependent on PolyMap and enable it again by config

--- src/main/java/io/github/theepicblock/polymc/mixins/CustomPacketDisabler.java
+++ src/main/java/io/github/theepicblock/polymc/mixins/CustomPacketDisabler.java
@@ -23,17 +23,21 @@ import io.netty.util.concurrent.GenericFutureListener;
 import net.minecraft.network.Packet;
 import net.minecraft.network.packet.s2c.play.CustomPayloadS2CPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public class CustomPacketDisabler {
+    @Shadow public ServerPlayerEntity player;
+
     @Inject(method = "sendPacket(Lnet/minecraft/network/Packet;Lio/netty/util/concurrent/GenericFutureListener;)V", at = @At("HEAD"), cancellable = true)
     public void sendPacketInject(Packet<?> packet, GenericFutureListener<? extends Future<? super Void>> listener, CallbackInfo ci) {
-        if (packet instanceof CustomPayloadS2CPacket) {
+        if (packet instanceof CustomPayloadS2CPacket && Util.isPolyMapVanillaLike(this.player)) {
             Identifier channel = ((CustomPacketAccessor) packet).getChannel();
             if (!Util.isVanilla(channel)) {
                 ci.cancel();
