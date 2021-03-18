a1449dacb8ade3c4e04f09f79c9e63e44c3770be
TheEpicBlock <git.teb@theepicblock.nl>
2021-03-18 19:34:38 +0100

    Wizard state

--- src/main/java/io/github/theepicblock/polymc/api/block/BlockWizard.java
+++ src/main/java/io/github/theepicblock/polymc/api/block/BlockWizard.java
@@ -2,7 +2,38 @@ package io.github.theepicblock.polymc.api.block;
 
 import io.github.theepicblock.polymc.impl.misc.WatchListener;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.Vec3d;
 
 public abstract class BlockWizard implements WatchListener {
+    private Vec3d position;
+    private WizardState state;
 
+    public BlockWizard(Vec3d position) {
+        this.position = position;
+    }
+
+    public abstract void onMove();
+    public abstract void onStateChange();
+
+    public final Vec3d getPosition() {
+        return position;
+    }
+
+    public final void setPosition(Vec3d position) {
+        this.position = position;
+        this.onMove();
+    }
+
+    public final WizardState getState() {
+        return state;
+    }
+
+    public final void setState(WizardState state) {
+        this.state = state;
+        this.onStateChange();
+    }
+
+    public enum WizardState {
+        BLOCK
+    }
 }
