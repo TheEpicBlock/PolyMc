bef1f3f7b22ecd9562553ad157bba335a81ff965
TheEpicBlock <git.teb@theepicblock.nl>
2021-07-17 20:15:19 +0200

    Generate binary blob containing block ids

--- datagen/src/main/java/io/github/theepicblock.polymc/datagen/Main.java
+++ datagen/src/main/java/io/github/theepicblock.polymc/datagen/Main.java
@@ -1,6 +1,15 @@
 package io.github.theepicblock.polymc.datagen;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.state.property.Property;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -18,15 +27,40 @@ public class Main implements ModInitializer {
             File outputDir = new File(output);
             outputDir.mkdirs();
 
+            PacketByteBuf outBuf = new PacketByteBuf(Unpooled.buffer());
             File outputFile = new File(outputDir, "block-ids");
             LOGGER.info("Output: "+outputFile.toPath().toAbsolutePath());
 
-            Files.writeString(outputFile.toPath(), "test", StandardOpenOption.CREATE);
+            outBuf.writeVarInt(Block.STATE_IDS.size());
+            for (BlockState state : Block.STATE_IDS) {
+                writeBlockstate(state, outBuf);
+            }
 
-            System.exit(0); // Shut down the server.
+            Files.write(outputFile.toPath(), outBuf.array(), StandardOpenOption.CREATE);
+
+            System.exit(0); // Shut down the server
         } catch (Exception e) {
             // Shutdown the server and tell Gradle it went wrongly
             System.exit(1);
         }
+    }
+
+    private static void writeBlockstate(BlockState state, PacketByteBuf buf) {
+        Identifier id = Registry.BLOCK.getId(state.getBlock());
+        assert id.getNamespace().equals("minecraft"); // This is supposed to be a list with vanilla ids, no modded allowed
+
+        // Write block id
+        buf.writeString(id.getPath());
+
+        buf.writeVarInt(state.getEntries().size());
+        state.getEntries().forEach((property, value) -> {
+            buf.writeString(property.getName());
+            buf.writeString(nameValue(property, value));
+        });
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T extends Comparable<T>> String nameValue(Property<T> property, Comparable<?> value) {
+        return property.name((T)value);
     }
 }
