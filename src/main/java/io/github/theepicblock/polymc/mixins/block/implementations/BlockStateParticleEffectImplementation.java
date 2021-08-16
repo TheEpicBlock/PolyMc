13d6c90d22c35e82e798e4c14d345057cde69214
TheEpicBlock <git.teb@theepicblock.nl>
2021-08-16 18:20:54 +0200

    Fix BlockStateParticleEffectImplementation mixin definition

--- src/main/java/io/github/theepicblock/polymc/mixins/block/implementations/BlockStateParticleEffectImplementation.java
+++ src/main/java/io/github/theepicblock/polymc/mixins/block/implementations/BlockStateParticleEffectImplementation.java
@@ -1,31 +1,29 @@
 package io.github.theepicblock.polymc.mixins.block.implementations;
 
-import io.github.theepicblock.polymc.PolyMc;
-import io.github.theepicblock.polymc.api.misc.PolyMapProvider;
+import io.github.theepicblock.polymc.impl.Util;
 import io.github.theepicblock.polymc.impl.mixin.PlayerContextContainer;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.particle.BlockStateParticleEffect;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.collection.IdList;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(BlockStateParticleEffect.class)
-public class BlockStateParticleEffectImplementation {
+public class BlockStateParticleEffectImplementation<T> {
     /**
      * Replaces the {@link net.minecraft.block.BlockState} that {@link net.minecraft.util.collection.IdList#getRawId(Object)} is called with
      */
-    @ModifyArg(method = "write", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/collection/IdList;getRawId(Ljava/lang/Object;)I"))
-    private BlockState redirectRawId(BlockState in, PacketByteBuf buf) {
+    @Redirect(method = "write", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/collection/IdList;getRawId(Ljava/lang/Object;)I"))
+    private int redirectRawId(IdList<T> idList, T in, PacketByteBuf buf) {
         ServerPlayerEntity player = ((PlayerContextContainer)buf).getPolyMcProvidedPlayer();
 
         if (player == null) {
             throw new NullPointerException("PacketByteBuf did not contain player context");
         }
 
-        return PolyMapProvider.getPolyMap(player).getClientBlock(in);
+        return Util.getPolydRawIdFromState((BlockState)in, player);
     }
 }
