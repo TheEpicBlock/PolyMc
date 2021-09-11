6aa678858b120ca4f2619e1f0894b5a772feca61
TheEpicBlock <git.teb@theepicblock.nl>
2021-09-11 21:00:43 +0200

    Add test block

--- src/testmod/java/nl/theepicblock/polymc/testmod/Testmod.java
+++ src/testmod/java/nl/theepicblock/polymc/testmod/Testmod.java
@@ -2,7 +2,12 @@ package nl.theepicblock.polymc.testmod;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.Material;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.text.LiteralText;
@@ -15,10 +20,13 @@ public class Testmod implements ModInitializer {
     private static final String MODID = "polymc-testmod";
 
     public static final Item TEST_ITEM = new TestItem(new FabricItemSettings().group(ItemGroup.MISC).maxCount(6).rarity(Rarity.EPIC));
-
+    public static final Block TEST_BLOCK = new TestBlock(FabricBlockSettings.of(Material.SOIL, MapColor.BLUE));
+    
     @Override
     public void onInitialize() {
         Registry.register(Registry.ITEM, id("test_item"), TEST_ITEM);
+        Registry.register(Registry.BLOCK, id("test_block"), TEST_BLOCK);
+        Registry.register(Registry.ITEM, id("test_block"), new BlockItem(TEST_BLOCK, new FabricItemSettings()));
     }
 
     public static void debugSend(@Nullable PlayerEntity playerEntity, String text) {
