4b728eab28ba49612dffebc4f94fb0a636097a13
TheEpicBlock <git.teb@theepicblock.nl>
2022-01-31 19:24:07 +0100

    Use correct direction type for JElement

--- src/main/java/io/github/theepicblock/polymc/api/resource/json/JElement.java
+++ src/main/java/io/github/theepicblock/polymc/api/resource/json/JElement.java
@@ -1,7 +1,5 @@
 package io.github.theepicblock.polymc.api.resource.json;
 
-import net.minecraft.util.math.Direction;
-
 import java.util.Map;
 
 @SuppressWarnings("ClassCanBeRecord") // Records don't work with GSON
@@ -10,9 +8,9 @@ public final class JElement {
     private final double[] to;
     private final JElementRotation rotation;
     private final boolean shade;
-    private final Map<Direction, JElementFace> faces;
+    private final Map<JDirection, JElementFace> faces;
 
-    public JElement(double[] from, double[] to, JElementRotation rotation, boolean shade, Map<Direction,JElementFace> faces) {
+    public JElement(double[] from, double[] to, JElementRotation rotation, boolean shade, Map<JDirection,JElementFace> faces) {
         this.from = from;
         this.to = to;
         this.rotation = rotation;
@@ -36,7 +34,7 @@ public final class JElement {
         return shade;
     }
 
-    public Map<Direction,JElementFace> faces() {
+    public Map<JDirection,JElementFace> faces() {
         return faces;
     }
 }
