8fdf21649941d08d46b8f7abb5d9cb285c2286cf
TheEpicBlock <git.teb@theepicblock.nl>
2022-06-03 23:11:46 +0200

    Fix #159

--- src/main/java/io/github/theepicblock/polymc/mixins/entity/DisableCustomEntities.java
+++ src/main/java/io/github/theepicblock/polymc/mixins/entity/DisableCustomEntities.java
@@ -1,31 +1,22 @@
 package io.github.theepicblock.polymc.mixins.entity;
 
 import io.github.theepicblock.polymc.api.misc.PolyMapProvider;
-import io.netty.util.concurrent.Future;
-import io.netty.util.concurrent.GenericFutureListener;
+import io.github.theepicblock.polymc.impl.mixin.EntityTrackerEntryDuck;
 import net.minecraft.network.Packet;
-import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket;
-import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.EntityTrackerEntry;
 import net.minecraft.server.network.ServerPlayerEntity;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(ServerPlayNetworkHandler.class)
+import java.util.function.Consumer;
+
+@Mixin(EntityTrackerEntry.class)
 public class DisableCustomEntities {
-    @Shadow
-    public ServerPlayerEntity player;
-
-    @Inject(method = "sendPacket(Lnet/minecraft/network/Packet;Lio/netty/util/concurrent/GenericFutureListener;)V", at = @At("HEAD"), cancellable = true)
-    private void sendPacketInject(Packet<?> packet, @Nullable GenericFutureListener<? extends Future<? super Void>> listener, CallbackInfo ci) {
-        if (packet instanceof EntitySpawnS2CPacket spawnPacket) {
-            var polyMap = PolyMapProvider.getPolyMap(this.player);
-            if (polyMap.getEntityPoly(spawnPacket.getEntityTypeId()) != null) {
-                ci.cancel();
-            }
+    @Redirect(method = "startTracking", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/EntityTrackerEntry;sendPackets(Ljava/util/function/Consumer;)V"))
+    public void polymc$maybeBlockSpawnPacket(EntityTrackerEntry instance, Consumer<Packet<?>> sender, ServerPlayerEntity player) {
+        if (((EntityTrackerEntryDuck)this).polymc$getWizards().get(PolyMapProvider.getPolyMap(player)) == null) {
+            instance.sendPackets(sender);
         }
     }
 }
