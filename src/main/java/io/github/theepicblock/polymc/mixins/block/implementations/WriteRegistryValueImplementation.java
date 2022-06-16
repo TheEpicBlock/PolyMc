24f3874dea3624bb3ef44a4e7da947243f3e5230
Jelle De Loecker <jelle@elevenways.be>
2022-06-16 23:10:00 +0200

    Restore the `PolyMap#getClientStateRawId` method

--- src/main/java/io/github/theepicblock/polymc/mixins/block/implementations/WriteRegistryValueImplementation.java
+++ src/main/java/io/github/theepicblock/polymc/mixins/block/implementations/WriteRegistryValueImplementation.java
@@ -8,18 +8,20 @@ import net.minecraft.util.collection.IndexedIterable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import xyz.nucleoid.packettweaker.PacketContext;
 
 @Mixin(PacketByteBuf.class)
 public class WriteRegistryValueImplementation {
-    @ModifyVariable(method = "writeRegistryValue", at = @At("HEAD"), argsOnly = true)
-    private <T> T redirectBlock(T original, IndexedIterable<T> registry) {
+
+    @Redirect(method = "writeRegistryValue", at=@At(value="INVOKE", target="Lnet/minecraft/util/collection/IndexedIterable;getRawId(Ljava/lang/Object;)I"))
+    private <T> int redirectGetRawId(IndexedIterable<T> registry, T value) {
         if (registry == Block.STATE_IDS) {
             var player = PacketContext.get().getTarget();
             var polymap = Util.tryGetPolyMap(player);
-            //noinspection unchecked
-            return (T)polymap.getClientState((BlockState)original, player);
+            return polymap.getClientStateRawId((BlockState) value, player);
         }
-        return original;
+
+        return registry.getRawId(value);
     }
 }
