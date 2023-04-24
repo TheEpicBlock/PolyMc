e238f445c65891fabf10054bc8c439a5e94b956e
TheEpicBlock <git.teb@theepicblock.nl>
2023-04-24 19:50:54 +0200

    Fix #207

--- src/main/java/io/github/theepicblock/polymc/impl/poly/entity/DefaultedEntityPoly.java
+++ src/main/java/io/github/theepicblock/polymc/impl/poly/entity/DefaultedEntityPoly.java
@@ -2,7 +2,6 @@ package io.github.theepicblock.polymc.impl.poly.entity;
 
 import io.github.theepicblock.polymc.api.entity.EntityPoly;
 import io.github.theepicblock.polymc.api.wizard.PacketConsumer;
-import io.github.theepicblock.polymc.api.wizard.VirtualEntity;
 import io.github.theepicblock.polymc.api.wizard.Wizard;
 import io.github.theepicblock.polymc.api.wizard.WizardInfo;
 import io.github.theepicblock.polymc.impl.poly.wizard.AbstractVirtualEntity;
@@ -10,10 +9,23 @@ import io.github.theepicblock.polymc.impl.poly.wizard.EntityUtil;
 import io.github.theepicblock.polymc.mixins.wizards.EntityAccessor;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.packet.s2c.play.EntityAttachS2CPacket;
+import net.minecraft.network.packet.s2c.play.EntityAttributesS2CPacket;
+import net.minecraft.network.packet.s2c.play.EntityEquipmentUpdateS2CPacket;
+import net.minecraft.network.packet.s2c.play.EntityPassengersSetS2CPacket;
+import net.minecraft.network.packet.s2c.play.EntityStatusEffectS2CPacket;
+import net.minecraft.network.packet.s2c.play.EntityTrackerUpdateS2CPacket;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import com.mojang.datafixers.util.Pair;
 
 public class DefaultedEntityPoly<T extends Entity> implements EntityPoly<T> {
     private final EntityType<?> displayType;
@@ -33,7 +45,7 @@ public class DefaultedEntityPoly<T extends Entity> implements EntityPoly<T> {
     }
 
     public static class DefaultedEntityWizard<T extends Entity> extends EntityWizard<T> {
-        private final VirtualEntity virtualEntity;
+        private final AbstractVirtualEntity virtualEntity;
 
         public DefaultedEntityWizard(WizardInfo info, T entity, EntityType<?> type) {
             super(info, entity);
@@ -51,7 +63,8 @@ public class DefaultedEntityPoly<T extends Entity> implements EntityPoly<T> {
 
         @Override
         public void addPlayer(PacketConsumer player) {
-            virtualEntity.spawn(player, this.getPosition());
+            var original = this.getEntity();
+            virtualEntity.spawn(player, this.getPosition(), original.getPitch(), original.getYaw(), 0, original.getVelocity());
 
             player.sendPacket(EntityUtil.createDataTrackerUpdate(
                     this.virtualEntity.getId(),
@@ -60,6 +73,51 @@ public class DefaultedEntityPoly<T extends Entity> implements EntityPoly<T> {
                             new DataTracker.Entry<>(EntityAccessor.getNameVisible(), true))
                     )
             );
+            
+            sendStandardPackets(player, getEntity());
+        }
+
+        public static void sendStandardPackets(PacketConsumer player, Entity original) {
+            var changedEntries = original.getDataTracker().getChangedEntries();
+            if (changedEntries != null && !changedEntries.isEmpty()) {
+                player.sendPacket(new EntityTrackerUpdateS2CPacket(original.getId(), changedEntries));
+            }
+
+            if (original instanceof LivingEntity e) {
+                var attributes = e.getAttributes().getAttributesToSend();
+                if (!attributes.isEmpty()) {
+                    player.sendPacket(new EntityAttributesS2CPacket(original.getId(), attributes));
+                }
+
+                var list = new ArrayList<Pair<EquipmentSlot, ItemStack>>();
+
+                for(var equipmentSlot : EquipmentSlot.values()) {
+                    var itemStack = e.getEquippedStack(equipmentSlot);
+                    if (!itemStack.isEmpty()) {
+                        list.add(Pair.of(equipmentSlot, itemStack.copy()));
+                    }
+                }
+
+                if (!list.isEmpty()) {
+                    player.sendPacket(new EntityEquipmentUpdateS2CPacket(e.getId(), list));
+                }
+
+                for(var statusEffect : e.getStatusEffects()) {
+                    player.sendPacket(new EntityStatusEffectS2CPacket(e.getId(), statusEffect));
+                }
+            }
+
+            if (!original.getPassengerList().isEmpty()) {
+                player.sendPacket(new EntityPassengersSetS2CPacket(original));
+            }
+    
+            if (original.hasVehicle()) {
+                player.sendPacket(new EntityPassengersSetS2CPacket(original.getVehicle()));
+            }
+    
+            if (original instanceof MobEntity mobEntity && mobEntity.isLeashed()) {
+                player.sendPacket(new EntityAttachS2CPacket(mobEntity, mobEntity.getHoldingEntity()));
+            }
         }
 
         @Override
