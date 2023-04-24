48a080c4bd5ac9ce367d85b2eaefc3a6af9b5a09
TheEpicBlock <git.teb@theepicblock.nl>
2023-04-24 20:02:05 +0200

    Fix #193

--- src/main/java/io/github/theepicblock/polymc/impl/poly/wizard/PacketCountManager.java
+++ src/main/java/io/github/theepicblock/polymc/impl/poly/wizard/PacketCountManager.java
@@ -40,7 +40,7 @@ public class PacketCountManager {
     private int cursor;
     private int watchDistance = 0;
     private int watchRadius = 0;
-    private final TrackingPacketConsumer reusableConsumer = new TrackingPacketConsumer();
+    private final ThreadLocal<TrackingPacketConsumer> reusableConsumer = ThreadLocal.withInitial(TrackingPacketConsumer::new);
 
     public static void registerEvents() {
         ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
@@ -90,7 +90,8 @@ public class PacketCountManager {
     }
 
     public PacketConsumer getView(Set<EntityTrackingListener> listeners, PolyMap map, Vec3d pos, int tick, int seed) {
-        this.reusableConsumer.reset(this.cursor);
+        var reusableConsumer = this.reusableConsumer.get();
+        reusableConsumer.reset(this.cursor);
 
         int pSeed = 0;
         for (var listener : listeners) {
@@ -107,7 +108,8 @@ public class PacketCountManager {
     }
 
     public PacketConsumer getView(ServerWorld world, ChunkPos pos, PolyMap map, int tick, int seed) {
-        this.reusableConsumer.reset(this.cursor);
+        var reusableConsumer = this.reusableConsumer.get();
+        reusableConsumer.reset(this.cursor);
         var chunkPos = new Vec3d(pos.getCenterX(), 0, pos.getCenterZ());
 
         int pSeed = 0;
