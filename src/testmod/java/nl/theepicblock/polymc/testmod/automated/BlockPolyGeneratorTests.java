ba41c90c51a99022692ab20109ddaaf5f53b35ea
TheEpicBlock <git.teb@theepicblock.nl>
2023-09-21 21:56:39 +0200

    Add test for slabs

--- src/testmod/java/nl/theepicblock/polymc/testmod/automated/BlockPolyGeneratorTests.java
+++ src/testmod/java/nl/theepicblock/polymc/testmod/automated/BlockPolyGeneratorTests.java
@@ -2,14 +2,13 @@ package nl.theepicblock.polymc.testmod.automated;
 
 import net.fabricmc.fabric.api.gametest.v1.FabricGameTest;
 import net.minecraft.block.*;
-import net.minecraft.registry.Registries;
 import net.minecraft.state.property.Properties;
 import net.minecraft.test.GameTest;
 import net.minecraft.test.TestContext;
 import net.minecraft.util.math.BlockPos;
 import nl.theepicblock.polymc.testmod.Testmod;
 
-import java.util.function.Predicate;
+import java.util.function.BiPredicate;
 
 public class BlockPolyGeneratorTests implements FabricGameTest {
     @GameTest(templateName = EMPTY_STRUCTURE)
@@ -17,13 +16,13 @@ public class BlockPolyGeneratorTests implements FabricGameTest {
         assertPoly(
                 ctx,
                 Testmod.TEST_DOOR,
-                result -> (result.getBlock() instanceof DoorBlock && opensAfterRightClick(ctx, result)),
+                (sState, cState) -> (cState.getBlock() instanceof DoorBlock && opensAfterRightClick(ctx, cState)),
                 "should be a door that's openable"
         );
         assertPoly(
                 ctx,
                 Testmod.TEST_IRON_DOOR,
-                result -> result.getBlock() == Blocks.IRON_DOOR,
+                (sState, cState) -> cState.getBlock() == Blocks.IRON_DOOR,
                 "as of 1.20.1, doors that can't be opened by hand (such as Testmod.TEST_IRON_DOOR), should only be polied with minecraft:iron_door"
         );
         ctx.complete();
@@ -34,22 +33,35 @@ public class BlockPolyGeneratorTests implements FabricGameTest {
         assertPoly(
                 ctx,
                 Testmod.TEST_TRAP_DOOR,
-                result -> (result.getBlock() instanceof TrapdoorBlock && opensAfterRightClick(ctx, result)),
+                (sState, cState) -> (cState.getBlock() instanceof TrapdoorBlock && opensAfterRightClick(ctx, cState)),
                 "should be a trap door that's openable"
         );
         assertPoly(
                 ctx,
                 Testmod.TEST_IRON_TRAP_DOOR,
-                result -> result.getBlock() == Blocks.IRON_TRAPDOOR,
+                (sState, cState) -> cState.getBlock() == Blocks.IRON_TRAPDOOR,
                 "as of 1.20.1, doors that can't be opened by hand (such as Testmod.TEST_IRON_DOOR), should only be polied with minecraft:iron_trapdoor"
         );
         ctx.complete();
     }
 
-    public static void assertPoly(TestContext ctx, Block a, Predicate<BlockState> check, String message) {
+    @GameTest(templateName = EMPTY_STRUCTURE)
+    public void testSlab(TestContext ctx) {
+        assertPoly(
+                ctx,
+                Testmod.TEST_SLAB,
+                (sState, cState) -> (sState.getCollisionShape(null, null).equals(cState.getCollisionShape(null, null))),
+                "slab should have matching collisions"
+        );
+        ctx.complete();
+    }
+
+    public static void assertPoly(TestContext ctx, Block a, BiPredicate<BlockState, BlockState> check, String message) {
         var poly = TestUtil.getMap().getBlockPoly(a);
-        var polied = poly.getClientBlock(a.getDefaultState());
-        ctx.assertTrue(check.test(polied), Registries.BLOCK.getId(a)+" didn't get polied correctly: "+message+ " found "+polied+" instead");
+        a.getStateManager().getStates().forEach(serverState -> {
+            var polied = poly.getClientBlock(serverState);
+            ctx.assertTrue(check.test(serverState, polied), serverState+" didn't get polied correctly: "+message+ " but found "+polied+" instead");
+        });
     }
 
     public static boolean opensAfterRightClick(TestContext ctx, BlockState a) {
